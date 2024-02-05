2a161c02b9d285f30593367215d6c490eb3b7403
Muhab Abubaker <mohababubakir2001@gmail.com>
2024-02-05 15:03:22 +0300

    ADD: dynamic queryset mapping using a callback

--- core/mixins.py
+++ core/mixins.py
@@ -1,16 +1,18 @@
 """
 An agregation of all DRF mixin class general-purposee utilities
 """
-
+from ast import Call
+from typing import Dict, Callable, Union
+import django
 from rest_framework import serializers
-
+from django.db.models import QuerySet, Model
 class SerializerMapperMixin:
     """
     utility mixin used to assign a different serializer class for each action
     """
 
     serializer_class = None
-    serializer_class_by_action = dict()
+    serializer_class_by_action: Dict[str, serializers.Serializer] = dict()
 
     def get_serializer_class(self) -> serializers.Serializer:
         if hasattr(self, "serializer_class_by_action"):
@@ -27,16 +29,27 @@ class QuerysetMapperMixin:
     """
 
     queryset = None
-    queryset_by_action = dict()
+    # a dictionary that either maps an action to a queryset or a callable that returns a queryset
+    queryset_by_action: Dict[str, Union[QuerySet, Callable]] = dict()
 
     def get_queryset(self):
         if hasattr(self, "queryset_by_action"):
-            queryset = self.queryset_by_action.get(self.action, self.queryset)
+            action_qs = self.queryset_by_action.get(self.action, self.queryset)
 
-            if self.queryset is None and queryset is None:
+            if callable(action_qs):
+                queryset = action_qs(self)
+            else:
+                queryset = action_qs
+
+            if self.queryset is None and action_qs is None:
                 print("WARNING: You may need to either provide a default queryset or add a queryset mapping to invoked method")
 
         return queryset
 
 
 
+def OwnedQuerySet(user_id: int, queryset: QuerySet, ownership_field: str = 'user'):
+    """
+        Queryset filter action that only returns owned objects by the current user
+    """
+    return queryset.filter(**{ownership_field: user_id})
