1529a8bf339ddea0ff2a9ed51af39abd75a30be2
Muhab Abubaker <mohababubakir2001@gmail.com>
2024-02-05 17:02:21 +0300

    FIX: type fix for the status field of HTTP response

--- core/serializers.py
+++ core/serializers.py
@@ -3,5 +3,5 @@ from rest_framework import serializers
 
 class HttpResponeSerializer(serializers.Serializer):
         message = serializers.CharField()
-        status = serializers.CharField()
+        status = serializers.IntegerField()
         data = serializers.CharField()
