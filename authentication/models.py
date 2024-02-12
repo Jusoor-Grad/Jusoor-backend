c57ad3e9e2f96e63cf9c295694856de9632b27ec
Muhab Abubaker <mohababubakir2001@gmail.com>
2024-02-12 14:47:50 +0300

    FIX: remove username uniqueness and applying email uniqueness instead

--- authentication/models.py
+++ authentication/models.py
@@ -9,13 +9,32 @@ from django.contrib.auth.models import AbstractUser
 
 from core.models import TimeStampedModel
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 class User(AbstractUser, TimeStampedModel):
     """
     Model to add extra fields to the default django user model
     """
     image = models.ImageField(upload_to='users', null=True, blank=True)
+
+
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=False,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    email = models.EmailField(verbose_name=_("email address"), blank=True, unique=True)
     
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self) -> str:
         return f'USERNAME: {self.username}, EMAIL: {self.email}'
