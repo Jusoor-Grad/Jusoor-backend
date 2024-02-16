06bc3294f0cc44ebe3b17a6f4dbb6bcedb07df3c
Muhab Abubaker <mohababubakir2001@gmail.com>
2024-02-16 23:22:16 +0300

    ADD: list endpoint for all patients

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 
 from rest_framework.routers import DefaultRouter
-from .views import KFUPMDeptViewset
+from .views import KFUPMDeptViewset, PatientViewSet
 
 router = DefaultRouter()
 
 router.register(r'kfupm-departments', KFUPMDeptViewset, basename='kfupm-departments')
+router.register(r'patients', PatientViewSet, basename='patients')
 
 urlpatterns = router.urls
--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
+from authentication.models import User
+from authentication.permissions import IsTherapist
+from authentication.serializers import HttpPatientListResponseSerializer, PatientReadSerializer
+from core.enums import UserRole
 from core.models import KFUPMDepartment
-from core.serializers import HttpKFUPMDepartmentDetailResponseSerializer, KFUPMDepartmentSerializer
+from core.serializers import HttpKFUPMDepartmentDetailResponseSerializer,  KFUPMDepartmentSerializer
 from core.viewssets import AugmentedViewSet
 from rest_framework.permissions import AllowAny
 from rest_framework.mixins import ListModelMixin
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
 
 class KFUPMDeptViewset(AugmentedViewSet, ListModelMixin):
     """Viewset for KFUPMDept"""
@@ -19,5 +24,21 @@ class KFUPMDeptViewset(AugmentedViewSet, ListModelMixin):
     filterset_fields = ['short_name', 'long_name']
 
     @swagger_auto_schema(responses={status.HTTP_200_OK: HttpKFUPMDepartmentDetailResponseSerializer()})
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+# TODO: add the patient sentiument profile in the future
+class PatientViewSet(AugmentedViewSet, ListModelMixin):
+
+    serializer_class = PatientReadSerializer
+
+    action_permissions = {
+        'list': [IsTherapist],
+    }
+
+    queryset = User.objects.filter(patient_profile__isnull=False).select_related('patient_profile__department')
+
+    @swagger_auto_schema(responses={status.HTTP_200_OK: HttpPatientListResponseSerializer()})
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
