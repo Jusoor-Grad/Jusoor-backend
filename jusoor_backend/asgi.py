c6a32ab2fe73704e96426654f52ac2a7c28c8616
Muhab Abubaker <mohababubakir2001@gmail.com>
2024-03-14 00:08:38 +0300

    fix(ws): remoce websockets [skip ci]

--- jusoor_backend/asgi.py
+++ jusoor_backend/asgi.py
@@ -13,23 +13,14 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jusoor_backend.settings')
 
 django_asgi_app = get_asgi_application()
 from authentication.middleware import JWTAuthMiddlewareStack
 
-import chat.routing
-
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    'websocket': AllowedHostsOriginValidator(
-        JWTAuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
-        )
-    )
+    
     # Just HTTP for now. (We can add other protocols later.)
 })
