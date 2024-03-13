62136c8eb706485d1c39214b8f8df9358c60821b
Muhab Abubaker <mohababubakir2001@gmail.com>
2024-03-13 22:26:37 +0300

    fix(http): adding pagination size query param

--- core/viewssets.py
+++ core/viewssets.py
@@ -3,8 +3,16 @@ from authentication.mixins import ActionBasedPermMixin
 
 from core.mixins import QuerysetMapperMixin, SerializerMapperMixin
 from core.renderer import FormattedJSONRenderrer
+from rest_framework.pagination import PageNumberPagination
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    page_query_param = 'page'
+    max_page_size = 10
 
 class AugmentedViewSet(SerializerMapperMixin, ActionBasedPermMixin, QuerysetMapperMixin, viewsets.GenericViewSet):
     
     renderer_classes = [FormattedJSONRenderrer]
     filterset_fields = None
+    pagination_class = CustomPagination
