a9ce38106ff1d363f2fe0ac5e8ae0b6bf7b6c1a8
Muhab Abubaker <mohababubakir2001@gmail.com>
2024-03-13 22:25:34 +0300

    feat(chat): quick mocks for chat mesages, rooms, and chat bots

---
+++ chat/mock.py
@@ -0,0 +1,58 @@
+import faker
+from authentication.models import User
+from faker import Faker
+from django.contrib.auth import get_user_model
+from chat.models import ChatBot, ChatMessage, ChatRoom
+
+faker = Faker()
+
+class ChatBotMocker:
+
+    def mock_instances(n: int):
+
+        bots = []
+        for i in range(n):
+            # create a user profile
+            user = User.objects.create_user(
+                username = faker.name(),
+                email = faker.email(),
+                password = faker.password()
+            )
+
+            bots.append(ChatBot(
+                user_profile=user,
+                name=faker.name(),
+                temperature= faker.random.random(),
+                max_response_tokens=faker.random.randint(200, 300),
+                model_name= 'gpt-3.5-turbo',
+                top_p=faker.random.random(),
+                captured_history_length=faker.random.randint(5, 20),
+                prompt=faker.text()
+            ))
+
+        return ChatBot.objects.bulk_create(bots)
+
+class ChatMessageMocker:
+
+    @staticmethod
+    def mock_instances(n_msg_pairs: int, user: User, bot: ChatBot):
+
+        chatroom, _ = ChatRoom.objects.get_or_create(user=user, bot=bot)
+        messages = []
+        for i in range(2*n_msg_pairs):
+            messages.extend([ChatMessage(
+                content=faker.text(),
+                sender=user,
+                receiver=bot.user_profile,
+                chat_room=chatroom
+            ),
+            ChatMessage(
+                content=faker.text(),
+                sender=bot.user_profile,
+                receiver=user,
+                chat_room=chatroom
+            )])
+
+        return ChatMessage.objects.bulk_create(messages)
+            
+
